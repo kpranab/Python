fa5a6d3723876427a462004e3bc0326236b41536
Pranab Kumar Sahoo <pranab7sahoo@gmail.com>
2019-04-21 12:17:06 +0530

    Generic views in Django

--- python-django-poc/newsite/mysite/books/views.py
+++ python-django-poc/newsite/mysite/books/views.py
@@ -1,20 +1,15 @@
-from django.http import Http404
-from django.shortcuts import render
+from django.views import generic
 
 from .models import Book
 
 
-def index(request):
-    all_books = Book.objects.all()
-    context = {
-        'all_books': all_books
-    }
-    return render(request, 'books/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'books/index.html'
+
+    def get_queryset(self):
+        return Book.objects.all()
 
 
-def detail(request, book_id):
-    try:
-        book = Book.objects.get(id=book_id)
-    except Book.DoesNotExist:
-        raise Http404('This book does not exists.')
-    return render(request, 'books/detail.html', {'book': book})
+class DetailView(generic.DetailView):
+    model = Book
+    template_name = 'books/detail.html'
