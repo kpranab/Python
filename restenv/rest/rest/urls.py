e08dbcb193869fac6cdd70170165014cedfff07e
Pranab Kumar Sahoo <pranab7sahoo@gmail.com>
2019-04-28 09:59:12 +0530

    File formated

--- restenv/rest/rest/urls.py
+++ restenv/rest/rest/urls.py
@@ -14,21 +14,21 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.conf.urls import include,url
+from django.conf.urls import include, url
 from rest_framework import routers
 from restapp.views import TaskViewSet
 from restapp import views
 from django.conf.urls.static import static
 from django.conf import settings
 
-#router = routers.DefaultRouter()
+# router = routers.DefaultRouter()
 router = routers.SimpleRouter()
-router.register(r'task',TaskViewSet)
-router.register(r'completed_task',views.CompletedTaskViewSet)
-router.register(r'due_task',views.DueTaskViewSet)
+router.register(r'task', TaskViewSet)
+router.register(r'completed_task', views.CompletedTaskViewSet)
+router.register(r'due_task', views.DueTaskViewSet)
 
 urlpatterns = [
-    url(r'^', include(router.urls)),
-    url('admin/', admin.site.urls),
+                  url(r'^', include(router.urls)),
+                  url('admin/', admin.site.urls),
 
-]+ static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
