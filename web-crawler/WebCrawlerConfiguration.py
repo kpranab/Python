11cacdd2e3216a2c97904cf5b38cb1721d702514
Pranab Kumar Sahoo <pranab7sahoo@gmail.com>
2019-05-05 06:39:09 +0530

    Creating directory for project

---
+++ web-crawler/WebCrawlerConfiguration.py
@@ -0,0 +1,10 @@
+import os
+
+
+def create_project_dir(directory):
+    if not os.path.exists(directory):
+        print('Creating the directory : ' + directory)
+        os.makedirs(directory)
+
+
+create_project_dir('thesite')
