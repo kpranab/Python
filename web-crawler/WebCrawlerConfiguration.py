ef02a98a12a5bf0e849e380ffdd9c94b32bee156
Pranab Kumar Sahoo <pranab7sahoo@gmail.com>
2019-05-05 07:01:11 +0530

    Create file and write data into file

--- web-crawler/WebCrawlerConfiguration.py
+++ web-crawler/WebCrawlerConfiguration.py
@@ -7,4 +7,15 @@ def create_project_dir(directory):
         os.makedirs(directory)
 
 
-create_project_dir('thesite')
+def create_data_files(project_name, base_url):
+    queue = os.path.join(project_name, 'queue.txt')
+    crawled = os.path.join(project_name, 'crawled.txt')
+    if not os.path.isfile(queue):
+        write_file(queue, base_url)
+    if not os.path.isfile(crawled):
+        write_file(crawled, '')
+
+
+def write_file(path, data):
+    with open(path, 'w') as f:
+        f.write(data)
