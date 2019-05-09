b10c8f3b861bf023f1caf3612da8fd16865fe3ec
Pranab Kumar Sahoo <pranab7sahoo@gmail.com>
2019-05-09 09:47:19 +0530

    Search automation using XPATH

--- selenium-poc/demo.py
+++ selenium-poc/demo.py
@@ -1,11 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 import time
 from time import sleep
 
 driver = webdriver.Chrome()
 driver.get("https://www.google.com/")
-elem = driver.find_element_by_name('q')
+elem = driver.find_element(By.XPATH,'//*[@id="tsf"]/div[2]/div/div[1]/div/div[1]/input')
 time.sleep(5)
 elem.clear()
 elem.send_keys("Python")
