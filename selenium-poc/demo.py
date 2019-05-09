31d07dc384db0412a0d9241bc0aa4d84ea6f8e86
Pranab Kumar Sahoo <pranab7sahoo@gmail.com>
2019-05-09 09:35:50 +0530

    Automate google serach page for searching text

--- selenium-poc/demo.py
+++ selenium-poc/demo.py
@@ -1,4 +1,12 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+from time import sleep
 
 driver = webdriver.Chrome()
 driver.get("https://www.google.com/")
+elem = driver.find_element_by_name('q')
+time.sleep(5)
+elem.clear()
+elem.send_keys("Python")
+elem.send_keys(Keys.RETURN)
