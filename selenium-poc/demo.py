372521dfd26ba10b5a0e8b27c4844aebfa74463f
Pranab Kumar Sahoo <pranab7sahoo@gmail.com>
2019-05-13 20:52:19 +0530

    Get url of a web page through automation

--- selenium-poc/demo.py
+++ selenium-poc/demo.py
@@ -5,8 +5,9 @@ import time
 from time import sleep
 
 driver = webdriver.Chrome()
-driver.get("https://www.wikipedia.org")
-elem = driver.find_element_by_tag_name('html')
-elem.send_keys(Keys.END)
+driver.get("https://www.google.com")
+elem = driver.find_element_by_link_text('About')
 time.sleep(5)
-elem.send_keys(Keys.HOME)
+elem.click()
+time.sleep(5)
+print(driver.current_url)
